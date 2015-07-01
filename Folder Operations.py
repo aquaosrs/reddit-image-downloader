84a9218205fe352a4ea09a3b3bc3241c6c6d3e47
AprilHickson <AprilHicksontagg@gmail.com>
2015-07-01 23:52:01 +0100

    Copied folder operations to their own folder

---
+++ Folder Operations.py
@@ -0,0 +1,34 @@
+def CreateDir(Path=Path):
+	# Check if directory exists and create it if not.
+	Path = Path.rstrip()
+	if not os.path.exists(Path):
+		try:
+			mkpath(Path)
+		except:
+			print "Cannot Create DIR"
+
+def DeleteDir(Path):
+	if not os.path.exists(Path):
+		try:
+			os.rmdir(Path)
+		except:
+			pass
+
+def RemoveEmptyFolders(path):
+	path = os.path.relpath(path)
+	if not os.path.isdir(path):
+		return
+
+	# remove empty subfolders
+	files = os.listdir(path)
+	if len(files):
+		for f in files:
+			fullpath = os.path.join(path, f)
+			if os.path.isdir(fullpath):
+				RemoveEmptyFolders(fullpath)
+
+	# if folder empty, delete it
+	files = os.listdir(path)
+	if len(files) == 0:
+		print "Removing empty folder:", path
+		DeleteDir(path)
